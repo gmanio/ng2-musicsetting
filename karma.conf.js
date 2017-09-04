545bf7f24e3fefba70c617ff5b8e330793e129aa
조아라(Ara Jo)/Platform개발3팀/SKTECH <araara.jo@sk.com>
2017-09-04 14:39:14 +0900

    [PF개발3팀/조아라] unit test 설정 및 테스트 코드 작성
    - test component에 unittest 작성

--- karma.conf.js
+++ karma.conf.js
@@ -7,27 +7,46 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular/cli'],
     plugins: [
       require('karma-jasmine'),
+      require('karma-coverage'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
+      require('karma-phantomjs-launcher'),
       require('@angular/cli/plugins/karma')
     ],
-    client:{
+    client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
+    files: [
+      {pattern: './src/test.ts', watched: false}
+    ],
+    preprocessors: {
+      './src/test.ts': ['@angular/cli']
+    },
+    mime: {
+      'text/x-typescript': ['ts', 'tsx']
+    },
+
     coverageIstanbulReporter: {
-      reports: [ 'html', 'lcovonly' ],
+      reports: ['html', 'lcovonly', 'cobertura', 'text-summary'],
+      'report-config': {
+        cobertura: {
+          file: 'cobertura.xml'
+        }
+      },
       fixWebpackSourcePaths: true
     },
     angularCli: {
       environment: 'dev'
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: config.angularCli && config.angularCli.codeCoverage
+      ? ['progress', 'coverage-istanbul']
+      : ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
+    autoWatch: false,
+    browsers: ['PhantomJS'],
     singleRun: false
   });
 };
